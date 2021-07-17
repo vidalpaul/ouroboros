22040984d14563bc6b00e5e8257ff3d1b4888e89
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-17 11:44:29 -0300

    toggle transaction data button

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,10 +1,47 @@
 import { MILLISECONDS_PY } from '../config';
 import Transaction from './Transaction';
+import Reac, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
-import React from 'react';
+const ToggleTransactionDisplay = ({ block }) => {
+  const [displayTransaction, setDisplayTransaction] = useState(false);
+  const { data } = block;
+
+  const toggleDisplayTransaction = () => {
+    setDisplayTransaction(!displayTransaction);
+  };
+
+  if (displayTransaction) {
+    return (
+      <div>
+        {data.map((transaction) => (
+          <div key={transaction.id}>
+            <hr />
+            <Transaction transaction={transaction} />
+          </div>
+        ))}
+        <br />
+        <Button variant='danger' size='sm' onClick={toggleDisplayTransaction}>
+          Show less
+        </Button>
+      </div>
+    );
+  }
+  if (block.hash === 'genesis_hash') {
+    return <></>;
+  }
+  return (
+    <div>
+      <br />
+      <Button variant='danger' size='sm' onClick={toggleDisplayTransaction}>
+        Show more
+      </Button>
+    </div>
+  );
+};
 
 const Block = ({ block }) => {
-  const { timestamp, hash, data } = block;
+  const { timestamp, hash } = block;
   const hashDisplay = `${hash.substring(0, 15)}...`;
   const timestampDisplay = new Date(
     timestamp / MILLISECONDS_PY
@@ -13,12 +50,7 @@ const Block = ({ block }) => {
     <div className='Block'>
       <div>Hash: {hashDisplay}</div>
       <div>Timestamp: {timestampDisplay}</div>
-      {data.map((transaction) => (
-        <div key={transaction.id}>
-          <hr />
-          <Transaction transaction={transaction} />
-        </div>
-      ))}
+      <ToggleTransactionDisplay block={block} />
     </div>
   );
 };
