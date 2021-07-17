7248bd045d0a4cd9060262167821ecc31d57e13a
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-17 13:50:51 -0300

    blockchain pagination endpoints

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -26,6 +26,17 @@ def route_default():
 def route_blockchain():
     return jsonify(blockchain.to_json())
 
+@app.route('/blockchain/range')
+def route_blockchain_range():
+    start = int(request.args.get('start'))
+    end = int(request.args.get('end'))
+
+    return jsonify(blockchain.to_json()[::-1][start:end])
+
+@app.route('/blockchain/length')
+def route_blockchain_length():
+    return jsonify(len(blockchain.chain))
+
 @app.route('/blockchain/mine')
 def route_blockchain_mine():
     transaction_data = transaction_pool.transaction_data()
@@ -79,7 +90,7 @@ if os.environ.get('PEER') == 'True':
         print(f'\n -- Error synchronizing: {e}')
 
 if os.environ.get('SEED_DATA') == 'True':
-    for i in range(100):
+    for i in range(10):
         blockchain.add_block([
             Transaction(Wallet(), Wallet().address, random.randint(2, 50)).to_json(),
             Transaction(Wallet(), Wallet().address, random.randint(2, 50)).to_json()
