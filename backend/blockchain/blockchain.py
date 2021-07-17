87b78bf8140e2e71cb6299e509362da881943d7d
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-16 22:11:26 -0300

    check historic balances

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,6 +1,7 @@
 from backend.blockchain.block import Block
 from backend.wallet.transaction import Transaction
 from backend.config import MINING_REWARD_INPUT
+from backend.wallet.wallet import Wallet
 
 class Blockchain:
     """
@@ -76,7 +77,8 @@ class Blockchain:
             - Each transaction must be valid.
         """
         transaction_ids = set()
-        for block in chain:
+        for i in range(len(chain)):
+            block = chain[i]
             has_mining_reward = False
             for transaction_json in block.data:
                 transaction = Transaction.from_json(transaction_json)
@@ -92,6 +94,16 @@ class Blockchain:
                 if transaction.id in transaction_ids:
                     raise Exception(f'Transaction {transaction.id} is not unique')
                 transaction_ids.add(transaction.id)
+
+                historic_blockchain = Blockchain()
+                historic_blockchain.chain = chain[0:i]
+                historic_balance = Wallet.calculate_balance(
+                    historic_blockchain,
+                    transaction.input['address']
+                )
+
+                if historic_balance != transaction.input['amount']:
+                    raise Exception(f'Transaction {transaction.id} has an invalid input amount')
 
                 Transaction.is_valid_transaction(transaction)
 
